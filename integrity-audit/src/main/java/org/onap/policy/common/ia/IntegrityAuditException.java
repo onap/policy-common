3295d0beb81bccac5e04decbd11ff19fe07d50dd
Temoc Rodriguez <cr056n@att.com>
2017-11-28 09:59:02 -0800

    Fix technical debt in common integrity-audit
    Address technical debt in policy/common integrity-audit submodule. Comment
    for empty constructors, remove useless assignment, use <> on constructor,
    use @Override. Not all technical debt addressed, only the ones specified
    by Pam in POLICY-336, except the use Set instead of HashSet where a
    method signature would change.
    
    Issue-ID: POLICY-456
    Change-Id: If8f9f4151503bdd22c8c405485538f8be779cabd
    Signed-off-by: Temoc Rodriguez <cr056n@att.com>

--- integrity-audit/src/main/java/org/onap/policy/common/ia/IntegrityAuditException.java
+++ integrity-audit/src/main/java/org/onap/policy/common/ia/IntegrityAuditException.java
@@ -23,6 +23,7 @@ package org.onap.policy.common.ia;
 public class IntegrityAuditException extends Exception{
 	private static final long serialVersionUID = 1L;
 	public IntegrityAuditException() {
+		// Empty constructor
 	}
 	public IntegrityAuditException(String message) {
 		super(message);
