c50aa5f5cde5ea4737c5b92eb1fdc91c2ebd1a29
Jim Hahn <jrh3@att.com>
2018-02-07 17:13:32 -0500

    Add ExtractAppender to facilite junit tests
    Added ExtractAppender to provide a way for junit tests to capture
    data sent to a logger.
    Changed logback.version to 1.2.3 and moved the property to the
    top-level pom.
    Updated license date in top-level pom.
    Refactored ExceptionsText, adding ErrorsTester and
    ThrowablesTester classes to reduce sonar issues.
    
    Change-Id: Ief7d08972bf4e7037b59c2afe4b77b252f2ad60a
    Issue-ID: POLICY-582
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ utils-test/src/main/java/org/onap/policy/common/utils/test/ConstructionError.java
@@ -0,0 +1,58 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Integrity Audit
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.test;
+
+/**
+ * An error that occurred while trying to construct an object for a junit test.
+ */
+public class ConstructionError extends AssertionError {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public ConstructionError() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public ConstructionError(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ConstructionError(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ConstructionError(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ utils-test/src/test/java/org/onap/policy/common/utils/test/ThrowablesTesterTest.java
@@ -0,0 +1,214 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Integrity Monitor
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ThrowablesTesterTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, new ThrowablesTester().testThrowable(SimpleThrowable.class));
+		assertEquals(5, new ThrowablesTester().testThrowable(StaticThrowable.class));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testIgnoreMessageThrowable() {
+		new ThrowablesTester().testThrowable(IgnoreMessageThrowable.class);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testIgnoreCauseThrowable() {
+		new ThrowablesTester().testThrowable(IgnoreCauseThrowable.class);
+	}
+
+	@Test
+	public void testNoConstructorsThrowable() {
+		assertEquals(0, new ThrowablesTester().testThrowable(NoConstructorsThrowable.class));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testAlwaysSuppressThrowable() {
+		new ThrowablesTester().testThrowable(AlwaysSuppressThrowable.class);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testNeverSuppressThrowable() {
+		new ThrowablesTester().testThrowable(NeverSuppressThrowable.class);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testAlwaysWritableThrowable() {
+		new ThrowablesTester().testThrowable(AlwaysWritableThrowable.class);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testNeverWritableThrowable() {
+		new ThrowablesTester().testThrowable(NeverWritableThrowable.class);
+	}
+
+	@Test(expected = ConstructionError.class)
+	public void testThrowInstantiationException() {
+		new ThrowablesTester().testThrowable(ThrowInstantiationException.class);
+	}
+
+	/**
+	 * Used to test a failure case - message text is ignored.
+	 */
+	public static class IgnoreMessageThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public IgnoreMessageThrowable(String message) {
+			super("bogus");
+		}
+	}
+
+	/**
+	 * Used to test a failure case - cause is ignored.
+	 */
+	public static class IgnoreCauseThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public IgnoreCauseThrowable(Throwable cause) {
+			super(new Throwable("another cause"));
+		}
+	}
+
+	/**
+	 * Used to test a failure case - this has no standard constructors. The only
+	 * constructor it has takes an "int", thus it is not one of the standard
+	 * constructors.
+	 */
+	public static class NoConstructorsThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public NoConstructorsThrowable(int value) {
+			super();
+		}
+	}
+
+	/**
+	 * Used to test a failure case - always suppresses.
+	 */
+	public static class AlwaysSuppressThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public AlwaysSuppressThrowable(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) {
+			super(message, cause, true, writableStackTrace);
+		}
+	}
+
+	/**
+	 * Used to test a failure case - never suppresses.
+	 */
+	public static class NeverSuppressThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public NeverSuppressThrowable(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) {
+			super(message, cause, false, writableStackTrace);
+		}
+	}
+
+	/**
+	 * Used to test a failure case - always allows stack writes.
+	 */
+	public static class AlwaysWritableThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public AlwaysWritableThrowable(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) {
+			super(message, cause, enableSuppression, true);
+		}
+	}
+
+	/**
+	 * Used to test a failure case - never allows stack writes.
+	 */
+	public static class NeverWritableThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public NeverWritableThrowable(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) {
+			super(message, cause, enableSuppression, false);
+		}
+	}
+
+	/**
+	 * Used to test a failure case - throws InstantiationException when
+	 * constructed.
+	 */
+	public static class ThrowInstantiationException extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public ThrowInstantiationException(String message, Throwable cause, boolean enableSuppression,
+				boolean writableStackTrace) throws InstantiationException {
+
+			throw new InstantiationException(ThrowablesTester.EXPECTED_EXCEPTION_MSG);
+		}
+	}
+
+	/**
+	 * Used to test a simple success case.
+	 */
+	public static class SimpleThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public SimpleThrowable() {
+			super();
+		}
+
+		public SimpleThrowable(String message) {
+			super(message);
+		}
+	}
+
+	/**
+	 * Used to test the exhaustive success case.
+	 */
+	public static class StaticThrowable extends Throwable {
+		private static final long serialVersionUID = 1L;
+
+		public StaticThrowable() {
+			super();
+		}
+
+		public StaticThrowable(String message) {
+			super(message);
+		}
+
+		public StaticThrowable(Throwable cause) {
+			super(cause);
+		}
+
+		public StaticThrowable(String message, Throwable cause) {
+			super(message, cause);
+		}
+
+		public StaticThrowable(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+			super(message, cause, enableSuppression, writableStackTrace);
+		}
+	}
+
+}
