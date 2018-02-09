dbb5ffe3732cf28e54a1202e28adffc53964437d
Jim Hahn <jrh3@att.com>
2018-02-09 12:20:56 -0500

    Add AutoClose wrappers for JPA classes
    Added EntityMgrCloser and EntityMgrTrans classes that will automatically
    close/rollback the wrapped JPA classes.  As a result, these classes can
    be used in try-with-resource statements to ensure that the JPA
    EntityTransaction is rolled back and the EntityManager is closed.
    Add "build" section into pom to make it constitent with other poms
    in common.
    
    Change-Id: I531f20d93a679d4bb432c1d75c1feac0e334bc44
    Issue-ID: POLICY-582
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ utils/src/main/java/org/onap/policy/common/utils/test/jpa/EntityTransCloser.java
@@ -0,0 +1,77 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * feature-session-persistence
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.test.jpa;
+
+import javax.persistence.EntityTransaction;
+
+/**
+ * Wrapper for an <i>EntityTransaction</i> that is auto-rolled back when closed.
+ * This is useful in try-with-resources statements.
+ */
+public class EntityTransCloser implements AutoCloseable {
+
+	/**
+	 * Transaction to be rolled back.
+	 */
+	private final EntityTransaction trans;
+
+	/**
+	 * Begins a transaction.
+	 * 
+	 * @param et
+	 *            transaction to wrap/begin
+	 */
+	public EntityTransCloser(EntityTransaction et) {
+		trans = et;
+		trans.begin();
+	}
+
+	/**
+	 * Gets the wrapped transaction.
+	 * 
+	 * @return the transaction
+	 */
+	public EntityTransaction getTransation() {
+		return trans;
+	}
+
+	/**
+	 * Commits the transaction.
+	 */
+	public void commit() {
+		trans.commit();
+	}
+
+	/**
+	 * Rolls back the transaction.
+	 */
+	public void rollback() {
+		trans.rollback();
+	}
+
+	@Override
+	public void close() {
+		if (trans.isActive()) {
+			trans.rollback();
+		}
+	}
+
+}
