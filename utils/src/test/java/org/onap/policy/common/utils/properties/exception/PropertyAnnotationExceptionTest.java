69f53ca1a4591afefa6bb7bd9ce9ca15b0994679
Jim Hahn <jrh3@att.com>
2018-03-23 14:47:42 -0400

    Add PropertyConfiguration class
    Added PropertyConfiguration class, which provides for the loading of
    properties into the fields of a subclass.  Annotations are used to
    identify the field to be loaded and the name of the property from
    which it is to be loaded.
    Changed attribute "useDefault" to "emptyOk" so that it only has to
    be specified when the default value is empty.
    Reformatted a couple of files.
    Added setAllFields() method.
    Added getRawPropertyValue() method so subclasses can change how
    values are extracted from the property set.
    Added SpecPropertyConfiguration so controller or session names
    may be embedded within property names.
    Fixed some typos.
    Refactored out generalizeType2().
    Changed exception classes to take field name instead of Field object.
    Modified getValue() to take the field name and class type instead of
    Field object.
    Removed or fixed comments in junit tests.
    Added more junit tests.
    Changed emptyOk attribute to accept, to facilitate adding other
    options in the future.
    
    Change-Id: I6b190fea687f87cf512fb3236bfcf40b7adc4566
    Issue-ID: POLICY-577
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ utils/src/test/java/org/onap/policy/common/utils/properties/exception/PropertyAnnotationExceptionTest.java
@@ -0,0 +1,70 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP Policy Engine - Common Modules
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.properties.exception;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class PropertyAnnotationExceptionTest extends BasicPropertyExceptionTester {
+
+    /**
+     * Test method for
+     * {@link org.onap.policy.common.utils.properties.exception.PropertyException#PropertyException(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public void testPropertyExceptionStringField() {
+        doTestPropertyExceptionStringField_AllPopulated(new PropertyAnnotationException(PROPERTY, FIELD));
+        doTestPropertyExceptionStringField_NullProperty(new PropertyAnnotationException(null, FIELD));
+        doTestPropertyExceptionStringField_NullField(new PropertyAnnotationException(PROPERTY, null));
+        doTestPropertyExceptionStringField_BothNull(new PropertyAnnotationException(null, null));
+    }
+
+    /**
+     * Test method for
+     * {@link org.onap.policy.common.utils.properties.exception.PropertyException#PropertyException(java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public void testPropertyExceptionStringFieldString() {
+        doTestPropertyExceptionStringFieldString(new PropertyAnnotationException(PROPERTY, FIELD, MESSAGE));
+    }
+
+    /**
+     * Test method for
+     * {@link org.onap.policy.common.utils.properties.exception.PropertyException#PropertyException(java.lang.String, java.lang.String, java.lang.Throwable)}.
+     */
+    @Test
+    public void testPropertyExceptionStringFieldThrowable() {
+        doTestPropertyExceptionStringFieldThrowable(new PropertyAnnotationException(PROPERTY, FIELD, THROWABLE));
+    }
+
+    /**
+     * Test method for
+     * {@link org.onap.policy.common.utils.properties.exception.PropertyException#PropertyException(java.lang.String, java.lang.String, java.lang.String, java.lang.Throwable)}.
+     */
+    @Test
+    public void testPropertyExceptionStringFieldStringThrowable() {
+        doTestPropertyExceptionStringFieldStringThrowable(
+                        new PropertyAnnotationException(PROPERTY, FIELD, MESSAGE, THROWABLE));
+    }
+
+}
