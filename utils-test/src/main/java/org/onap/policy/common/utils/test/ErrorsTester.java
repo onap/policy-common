fc6d9f477a3dd7779e63501d10803e1819a109f1
vamshi.nemalikonda <vn00480215@techmahindra.com>
2018-06-07 17:53:14 +0530

    Removing checkstyle warnings from ErrorsTester
    Change-Id: Ifc70d9682317033fd98b43ba802f9fd1acf519a8
    Issue-ID: POLICY-881
    Signed-off-by: vamshi.nemalikonda <vn00480215@techmahindra.com>

--- utils-test/src/main/java/org/onap/policy/common/utils/test/ErrorsTester.java
+++ utils-test/src/main/java/org/onap/policy/common/utils/test/ErrorsTester.java
@@ -1,21 +1,21 @@
 /*
- * ============LICENSE_START=======================================================
+ * ============LICENSE_START====================================================
  * Common Utils-Test
- * ================================================================================
+ * =============================================================================
  * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
- * ================================================================================
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
+ *
+ * * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
+ * ============LICENSE_END======================================================
  */
 
 package org.onap.policy.common.utils.test;
@@ -27,20 +27,22 @@ package org.onap.policy.common.utils.test;
 public class ErrorsTester extends ThrowablesTester {
 
 	/**
-	 * Runs tests, on an Error subclass, for all of the standard constructors.
-	 * If the Error subclass does not support a given type of constructor, then
-	 * it skips that test. Does <i>not</i> throw an exception if no standard
-	 * constructors are found.
-	 * 
-	 * @param claz
-	 *            subclass to be tested
+	 * Runs tests, on an Error subclass, for all of the standard
+	 * constructors.If the Error subclass does not support a given
+	 * type of constructor, then it skips that test.
+	 * Does <i>not</i> throw an exception if no standard constructors
+	 * are found.
+	 *
+	 * @param claz subclass to be tested
+	 * @param <T> this needs to be declared
+	 *
 	 * @return the number of constructors that were found/tested
 	 * @throws ConstructionError
 	 *             if the Error subclass cannot be constructed
 	 * @throws AssertionError
 	 *             if the constructed objects fail to pass various tests
 	 */
-	public <T extends Error> int testAllError(Class<T> claz) {
+	public <T extends Error> int testAllError(final Class<T> claz) {
 		return testAllThrowable(claz);
 	}
 }
