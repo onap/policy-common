48db96b20ef29a4bcf871edbed43516c52db5ac7
Krishnakumar Jinka <kris.jinka@samsung.com>
2018-11-25 12:27:05 +0900

    Fix checkstyle issue in policy common
    Move method into class it is being called. Fix minor checkstyle
    formatting in the class. The changes have side effects hence
    revert some changes.
    
    Issue-ID: POLICY-1251
    Change-Id: Iecb513dafe3f60628664f3e60d6c49d3e4ce3148
    Signed-off-by: kris.jinka <kris.jinka@samsung.com>

--- integrity-monitor/src/test/java/org/onap/policy/common/im/StateManagementTest.java
+++ integrity-monitor/src/test/java/org/onap/policy/common/im/StateManagementTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,6 +49,7 @@ import org.slf4j.LoggerFactory;
  * privileges and can execute time-sensitive tasks.
  */
 public class StateManagementTest extends IntegrityMonitorTestBase {
+
     private static final String TEST_RESOURCE_NAME = "test_resource1";
     private static Logger logger = LoggerFactory.getLogger(StateManagementTest.class);
     //
@@ -276,8 +277,8 @@ public class StateManagementTest extends IntegrityMonitorTestBase {
 
     /**
      * Converts a state element to a comma-separated string.
-     * 
-     * @param se element to be converted
+     *
+     * @param sm element to be converted
      * @return a string representing the element
      */
     private String makeString(final StateManagement sm) {
