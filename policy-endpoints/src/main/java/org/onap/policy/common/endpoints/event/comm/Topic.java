ecbcce8b895a6c17f3a0b2b9928b1ca3868765ae
Krzysztof Opasiak <k.opasiak@samsung.com>
2019-01-28 21:11:38 +0100

    Remove constant from Topic interface
    Fix sonar issue:
    
    Move constants to a class or enum
    
    Issue-ID: POLICY-1486
    Depends-On: I453a14c3dda337f2a4d08914ead32b37adc17353
    Change-Id: I6baac1fe6e692074bca971f3c35bb2a29cb49205
    Signed-off-by: Krzysztof Opasiak <k.opasiak@samsung.com>

--- policy-endpoints/src/main/java/org/onap/policy/common/endpoints/event/comm/Topic.java
+++ policy-endpoints/src/main/java/org/onap/policy/common/endpoints/event/comm/Topic.java
@@ -3,13 +3,14 @@
  * policy-endpoints
  * ================================================================================
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Modified Copyright (C) 2019 Samsung Electronics Co., Ltd.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,11 +31,6 @@ import org.onap.policy.common.capabilities.Startable;
  * Essential Topic Data.
  */
 public interface Topic extends TopicRegisterable, Startable, Lockable {
-
-    /**
-     * network logger.
-     */
-    public static final String NETWORK_LOGGER = "network";
 
     /**
      * Underlying Communication infrastructure Types.
@@ -60,28 +56,28 @@ public interface Topic extends TopicRegisterable, Startable, Lockable {
 
     /**
      * Gets the topic name.
-     * 
+     *
      * @return topic name
      */
     public String getTopic();
 
     /**
      * Gets the communication infrastructure type.
-     * 
+     *
      * @return CommInfrastructure object
      */
     public CommInfrastructure getTopicCommInfrastructure();
 
     /**
      * Return list of servers.
-     * 
+     *
      * @return bus servers
      */
     public List<String> getServers();
 
     /**
      * Get the more recent events in this topic entity.
-     * 
+     *
      * @return list of most recent events
      */
     public String[] getRecentEvents();
