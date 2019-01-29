928385e46f860d7d8c7eb3ccb5b0f4a2a020e820
Krzysztof Opasiak <k.opasiak@samsung.com>
2019-01-29 23:12:46 +0100

    Don't reinit allowSelfSignedCerts
    allowselfsignedcerts field is defined and initialized in BusTopicBase
    so there is need to overwrite it once agin with the same value.
    
    Change-Id: Id0a389921954606392eb2cb3c23cd0d8ee3dc4ae
    Issue-ID: POLICY-1387
    Signed-off-by: Krzysztof Opasiak <k.opasiak@samsung.com>

--- policy-endpoints/src/main/java/org/onap/policy/common/endpoints/event/comm/bus/internal/SingleThreadedUebTopicSource.java
+++ policy-endpoints/src/main/java/org/onap/policy/common/endpoints/event/comm/bus/internal/SingleThreadedUebTopicSource.java
@@ -8,9 +8,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,15 +32,12 @@ public class SingleThreadedUebTopicSource extends SingleThreadedBusTopicSource i
 
     /**
      * Constructor.
-     * 
+     *
      * @param busTopicParams Parameters object containing all the required inputs
      * @throws IllegalArgumentException An invalid parameter passed in
      */
     public SingleThreadedUebTopicSource(BusTopicParams busTopicParams) {
-
         super(busTopicParams);
-
-        this.allowSelfSignedCerts = busTopicParams.isAllowSelfSignedCerts();
 
         this.init();
     }
@@ -67,7 +64,6 @@ public class SingleThreadedUebTopicSource extends SingleThreadedBusTopicSource i
     public CommInfrastructure getTopicCommInfrastructure() {
         return Topic.CommInfrastructure.UEB;
     }
-
 
     @Override
     public String toString() {
