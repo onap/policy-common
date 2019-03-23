c7c198cbfb5737ceb7e0fe11da0887720ccb6eab
Jim Hahn <jrh3@att.com>
2019-03-23 09:22:30 -0400

    Add DAO interface
    Added an interface to hide JPA implementations.
    After reviewing the way the models repo works, the pakcage was renamed
    from "provider" to "dao".
    Added more comments to the Dao class.
    Fixed checkstyle issue with comment.
    
    Added more comments.
    
    Change-Id: Iebd47b1474a34c38f676c3c331677377a43c9a4b
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- utils/src/main/java/org/onap/policy/common/utils/jpa/EntityMgrCloser.java
+++ utils/src/main/java/org/onap/policy/common/utils/jpa/EntityMgrCloser.java
@@ -2,14 +2,14 @@
  * ============LICENSE_START=======================================================
  * Common Utils
  * ================================================================================
- * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018-2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,12 +21,14 @@
 package org.onap.policy.common.utils.jpa;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import org.onap.policy.common.utils.dao.Dao;
 
 /**
  * Wrapper for an <i>EntityManager</i>, providing auto-close functionality. This is useful in
  * try-with-resources statements.
  */
-public class EntityMgrCloser implements AutoCloseable {
+public class EntityMgrCloser implements Dao {
 
     /**
      * The wrapped manager.
@@ -35,7 +37,7 @@ public class EntityMgrCloser implements AutoCloseable {
 
     /**
      * Construct an instance with the EntityManager.
-     * 
+     *
      * @param em manager to be auto-closed
      */
     public EntityMgrCloser(EntityManager em) {
@@ -44,7 +46,7 @@ public class EntityMgrCloser implements AutoCloseable {
 
     /**
      * Gets the EntityManager wrapped within this object.
-     * 
+     *
      * @return the associated EntityManager
      */
     public EntityManager getManager() {
@@ -53,7 +55,19 @@ public class EntityMgrCloser implements AutoCloseable {
 
     @Override
     public void close() {
-        em.close();
+        try {
+            EntityTransaction trans = em.getTransaction();
+            if (trans.isActive()) {
+                trans.rollback();
+            }
+
+        } finally {
+            em.close();
+        }
     }
 
+    @Override
+    public EntityTransCloser beginTransaction() {
+        return new EntityTransCloser(em.getTransaction());
+    }
 }
