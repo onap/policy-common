b78da92648868a2cd4bfc484e1e0d99f2b9faf48
Jim Hahn <jrh3@att.com>
2019-09-09 16:51:32 -0400

    Create StandardYamlCoder
    Created StandardYamlCoder which is like a StandardCoder, except that
    the former converts to/from YAML, while the latter converts to/from JSON.
    Also added YamlMessageBodyHandler and incorporated it into the http server
    so that it supports a media type of */yaml.
    
    Change-Id: Ibd83a9f6d355a330f63e435f2bb41affcf1947c2
    Issue-ID: POLICY-2065
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ policy-endpoints/src/main/java/org/onap/policy/common/endpoints/http/server/YamlMessageBodyHandler.java
@@ -0,0 +1,110 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.endpoints.http.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.onap.policy.common.utils.coder.StandardYamlCoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provider that serializes and de-serializes JSON via gson.
+ */
+@Provider
+@Consumes(MediaType.WILDCARD)
+@Produces(MediaType.WILDCARD)
+public class YamlMessageBodyHandler implements MessageBodyReader<Object>, MessageBodyWriter<Object> {
+
+    public static final Logger logger = LoggerFactory.getLogger(YamlMessageBodyHandler.class);
+
+    /**
+     * Constructs the object.
+     */
+    public YamlMessageBodyHandler() {
+        logger.info("Accepting YAML for REST calls");
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return canHandle(mediaType);
+    }
+
+    @Override
+    public long getSize(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                    MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException {
+
+        try (OutputStreamWriter writer = new OutputStreamWriter(entityStream, StandardCharsets.UTF_8)) {
+            new StandardYamlCoder().encode(writer, object);
+
+        } catch (CoderException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return canHandle(mediaType);
+    }
+
+    /**
+     * Determines if this provider can handle the given media type.
+     *
+     * @param mediaType the media type of interest
+     * @return {@code true} if this provider handles the given media type, {@code false}
+     *         otherwise
+     */
+    private boolean canHandle(MediaType mediaType) {
+        return (mediaType != null && "yaml".equalsIgnoreCase(mediaType.getSubtype()));
+    }
+
+    @Override
+    public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                    MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException {
+
+        try (InputStreamReader streamReader = new InputStreamReader(entityStream, StandardCharsets.UTF_8)) {
+            Class<?> clazz = (Class<?>) genericType;
+            return new StandardYamlCoder().decode(streamReader, clazz);
+
+        } catch (CoderException e) {
+            throw new IOException(e);
+        }
+    }
+}
---
+++ policy-endpoints/src/test/java/org/onap/policy/common/endpoints/http/server/test/MyYamlProvider.java
@@ -0,0 +1,82 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.endpoints.http.server.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import lombok.AccessLevel;
+import lombok.Setter;
+import org.onap.policy.common.endpoints.http.server.YamlMessageBodyHandler;
+
+/**
+ * GsonMessageBodyHandler that tracks activities.
+ */
+public class MyYamlProvider extends YamlMessageBodyHandler {
+
+    @Setter(AccessLevel.PRIVATE)
+    private static boolean readSome = false;
+
+    @Setter(AccessLevel.PRIVATE)
+    private static boolean wroteSome = false;
+
+    /**
+     * Constructs the object and resets the variables to indicate that no activity has
+     * occurred yet.
+     */
+    public MyYamlProvider() {
+        super();
+    }
+
+    @Override
+    public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                    MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException {
+
+        setReadSome(true);
+        return super.readFrom(type, genericType, annotations, mediaType, httpHeaders, entityStream);
+    }
+
+    @Override
+    public void writeTo(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                    MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException {
+
+        setWroteSome(true);
+        super.writeTo(object, type, genericType, annotations, mediaType, httpHeaders, entityStream);
+    }
+
+    public static boolean hasReadSome() {
+        return readSome;
+    }
+
+    public static boolean hasWrittenSome() {
+        return wroteSome;
+    }
+
+    public static void resetSome() {
+        MyYamlProvider.readSome = false;
+        MyYamlProvider.wroteSome = false;
+    }
+
+}
