e979067917b195094d0014bceef8252ce6dfb2aa
liamfallon <liam.fallon@est.tech>
2019-11-01 11:21:54 +0000

    Add common Jersey version
    Add a common version of Jersey for all Policy Framework components.
    
    Issue-ID: POLICY-2209
    Change-Id: I8554a97e9f13887418118966bfe1043b65d9a179
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- policy-endpoints/src/test/java/org/onap/policy/common/endpoints/http/server/test/MyJacksonProvider.java
+++ policy-endpoints/src/test/java/org/onap/policy/common/endpoints/http/server/test/MyJacksonProvider.java
@@ -3,6 +3,7 @@
  * ONAP
  * ================================================================================
  * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +21,6 @@
 
 package org.onap.policy.common.endpoints.http.server.test;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -28,8 +28,11 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+
 import lombok.AccessLevel;
 import lombok.Setter;
+
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
 
 /**
  * JacksonJsonProvider that tracks activities.
