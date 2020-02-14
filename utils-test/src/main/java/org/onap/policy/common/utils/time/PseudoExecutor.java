0cad58cbf2229bb0d8843caf6cf4d0606ab88d79
Jim Hahn <jrh3@att.com>
2020-02-14 16:29:11 -0500

    Add PseudoExecutor to utils-test
    Moved MyExec from actors package to policy-common/utils-test,
    renaming it to PseudoExecutor.
    
    Updated per review comment(s):
    - declare "task" of type "Queue" instead of "LinkedList"
    
    Issue-ID: POLICY-2363
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: I40049ca52defecc4f3f7479470c28da58521866d

---
+++ utils-test/src/main/java/org/onap/policy/common/utils/time/PseudoExecutor.java
@@ -0,0 +1,70 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.time;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+import lombok.Getter;
+
+/**
+ * Executor that will run tasks until the queue is empty or a maximum number of tasks have
+ * been executed. Doesn't actually run anything until {@link #runAll()} is invoked.
+ */
+public class PseudoExecutor implements Executor {
+
+    /**
+     * Tasks to be run.
+     */
+    @Getter
+    private final Queue<Runnable> tasks = new LinkedList<>();
+
+
+    /**
+     * Gets the queue length.
+     *
+     * @return the queue length
+     */
+    public int getQueueLength() {
+        return tasks.size();
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        tasks.add(command);
+    }
+
+    /**
+     * Runs all tasks until the queue is empty or the maximum number of tasks have been
+     * reached.
+     *
+     * @param maxTasks maximum number of tasks to run
+     * @return {@code true} if the queue is empty, {@code false} if the maximum number of
+     *         tasks have been reached before the queue was emptied
+     */
+    public boolean runAll(int maxTasks) {
+        for (int count = 0; count < maxTasks && !tasks.isEmpty(); ++count) {
+            tasks.remove().run();
+        }
+
+        return tasks.isEmpty();
+    }
+}
---
+++ utils-test/src/test/java/org/onap/policy/common/utils/time/PseudoExecutorTest.java
@@ -0,0 +1,60 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.time;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PseudoExecutorTest {
+    private int invoked;
+    private PseudoExecutor executor;
+
+    @Before
+    public void setUp() {
+        invoked = 0;
+        executor = new PseudoExecutor();
+    }
+
+    @Test
+    public void test() {
+        assertEquals(0, executor.getQueueLength());
+        assertEquals(0, executor.getTasks().size());
+        assertTrue(executor.runAll(0));
+
+        executor.execute(() -> invoked++);
+        executor.execute(() -> invoked++);
+        executor.execute(() -> invoked++);
+        assertEquals(3, executor.getTasks().size());
+        assertEquals(3, executor.getQueueLength());
+
+        assertFalse(executor.runAll(2));
+        assertEquals(2, invoked);
+        assertEquals(1, executor.getQueueLength());
+
+        assertTrue(executor.runAll(2));
+        assertEquals(3, invoked);
+        assertEquals(0, executor.getQueueLength());
+    }
+}
