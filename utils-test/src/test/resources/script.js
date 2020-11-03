b0a1a6791184ce25e230ca58c926a63346390b3b
Jim Hahn <jrh3@att.com>
2020-11-03 09:25:34 -0500

    Experiment with stringify
    Issue-ID: POLICY-2463
    Change-Id: Ib7f72d8379836c33d5518fb97fcae42f4839b0ad
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ utils-test/src/test/resources/script.js
@@ -0,0 +1,10 @@
+
+//JSON.stringify(new java.lang.String("some text"));
+
+//JSON.stringify(new java.lang.Integer(20));
+
+JSON.stringify(abc[0]["hello"]);
+
+//JSON.stringify(abc[0]);
+
+//JSON.stringify(abc);
