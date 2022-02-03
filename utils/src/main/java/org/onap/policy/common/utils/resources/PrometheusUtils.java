56a1d2bf3c2154e2b92681f8c3ae55ac082b6dba
jhh <jorge.hernandez-herrero@att.com>
2022-02-02 20:23:03 -0600

    add global constants for prometheus usage
    Issue-ID: POLICY-3761
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: I3f11d2fdb49d5b38d51add86ab6989e5d93584e9

---
+++ utils/src/main/java/org/onap/policy/common/utils/resources/PrometheusUtils.java
@@ -0,0 +1,68 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2022 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.common.utils.resources;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Prometheus constants and utilities.
+ */
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PrometheusUtils {
+
+    /**
+     * Policy Deployments Metric Name.
+     */
+    public static final String POLICY_DEPLOYMENTS_METRIC = "policy_deployments";
+
+    /**
+     * Policy Deployments Metric Help Message.
+     */
+    public static final String POLICY_DEPLOYMENT_HELP = "The total number of policy deployments.";
+
+    /**
+     * Metric label for arbitrary operations (eg. deploy, undeploy, execute).
+     */
+    public static final String OPERATION_METRIC_LABEL = "operation";
+
+    /**
+     * Deploy operation value.
+     */
+    public static final String DEPLOY_OPERATION = "deploy";
+
+    /**
+     * Undeploy operation value.
+     */
+    public static final String UNDEPLOY_OPERATION = "undeploy";
+
+    /**
+     * Metric label for states (ie. PASSIVE, ACTIVE).
+     */
+    public static final String STATE_METRIC_LABEL = "state";
+
+    /**
+     * Metric label for status of an operation (ie. SUCCESS or FAILURE).
+     */
+    public static final String STATUS_METRIC_LABEL = "status";
+
+}
