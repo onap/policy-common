e741dac9df01eb66ab194e8ca18a1ad54b1da044
Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
2022-08-25 15:00:58 +0100

    [POLICY] Publish and Subscribe to Kafka topic
    Issue-ID: POLICY-4134
    Signed-off-by: Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
    Change-Id: Idefa5b6f3cb702a4b478b76570717e73214d235a

--- policy-endpoints/src/main/java/org/onap/policy/common/endpoints/utils/KafkaPropertyUtils.java
+++ policy-endpoints/src/main/java/org/onap/policy/common/endpoints/utils/KafkaPropertyUtils.java
@@ -24,9 +24,13 @@ package org.onap.policy.common.endpoints.utils;
 import com.google.re2j.Pattern;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.onap.policy.common.endpoints.event.comm.bus.internal.BusTopicParams;
 import org.onap.policy.common.endpoints.event.comm.bus.internal.BusTopicParams.TopicParamsBuilder;
 import org.onap.policy.common.endpoints.properties.PolicyEndPointProperties;
@@ -34,6 +38,26 @@ import org.onap.policy.common.endpoints.properties.PolicyEndPointProperties;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KafkaPropertyUtils {
     private static final Pattern COMMA_SPACE_PAT = Pattern.compile("\\s*,\\s*");
+
+    /**
+     * Maps a topic property to a DME property.
+     */
+    private static final Map<String, String> PROP_TO_SASL;
+
+    static {
+        Map<String, String> map = new HashMap<>();
+
+        map.put(PolicyEndPointProperties.PROPERTY_KAFKA_SECURITY_PROTOCOL,
+                        PolicyEndPointProperties.DEFAULT_KAFKA_SECURITY_PROTOCOL);
+
+        map.put(PolicyEndPointProperties.PROPERTY_KAFKA_SASL_MECHANISM,
+                        PolicyEndPointProperties.DEFAULT_KAFKA_SASL_MECHANISM);
+
+        map.put(PolicyEndPointProperties.PROPERTY_KAFKA_JAAS_CONFIG,
+                        PolicyEndPointProperties.DEFAULT_KAFKA_JAAS_CONFIG);
+
+        PROP_TO_SASL = Collections.unmodifiableMap(map);
+    }
 
     /**
      * Makes a topic builder, configuring it with properties that are common to both
@@ -46,13 +70,27 @@ public class KafkaPropertyUtils {
      */
     public static TopicParamsBuilder makeBuilder(PropertyUtils props, String topic, String servers) {
 
+        /* Additional Kafka security Properties */
+        Map<String, String> kafkaAdditionalProps = new HashMap<>();
+
+        for (Map.Entry<String, String> ent : PROP_TO_SASL.entrySet()) {
+            String propName = ent.getKey();
+            var value = props.getString(propName, null);
+
+            if (!StringUtils.isBlank(value)) {
+                String dmeName = ent.getValue();
+                kafkaAdditionalProps.put(dmeName, value);
+            }
+        }
+
         final List<String> serverList = new ArrayList<>(Arrays.asList(COMMA_SPACE_PAT.split(servers)));
-        //TODO More Kafka properties to be added
+
         return BusTopicParams.builder()
                     .servers(serverList)
                     .topic(topic)
                     .effectiveTopic(props.getString(PolicyEndPointProperties.PROPERTY_TOPIC_EFFECTIVE_TOPIC_SUFFIX,
                                     topic))
+                    .additionalProps(kafkaAdditionalProps)
                     .managed(props.getBoolean(PolicyEndPointProperties.PROPERTY_MANAGED_SUFFIX, true));
     }
 }
