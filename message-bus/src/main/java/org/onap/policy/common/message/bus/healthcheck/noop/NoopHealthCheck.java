71329ff7d27a166c874aac04b5501b126fe60d96
adheli.tavares <adheli.tavares@est.tech>
2025-08-14 11:11:44 +0100

    Fix noop health check to analise if topics are alive
    Issue-ID: POLICY-5445
    Change-Id: I4a004ebced4cd4e31e84fbdfae93823e8f986b4b
    Signed-off-by: adheli.tavares <adheli.tavares@est.tech>

--- message-bus/src/main/java/org/onap/policy/common/message/bus/healthcheck/noop/NoopHealthCheck.java
+++ message-bus/src/main/java/org/onap/policy/common/message/bus/healthcheck/noop/NoopHealthCheck.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- * Copyright (C) 2025 Nordix Foundation.
+ * Copyright (C) 2025 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,50 @@
 
 package org.onap.policy.common.message.bus.healthcheck.noop;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.onap.policy.common.message.bus.event.Topic;
+import org.onap.policy.common.message.bus.event.TopicEndpoint;
+import org.onap.policy.common.message.bus.event.TopicEndpointManager;
 import org.onap.policy.common.message.bus.healthcheck.TopicHealthCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NoopHealthCheck implements TopicHealthCheck {
 
+    private final Logger logger = LoggerFactory.getLogger(NoopHealthCheck.class);
+
+    private final TopicEndpoint topicEndpoint = TopicEndpointManager.getManager();
+
+    private Map<String, Topic> actualTopics;
+
     @Override
     public boolean healthCheck(List<String> topics) {
-        return true;
+        var topicsHealthy = true;
+
+        this.populateActualTopics();
+
+        if (topicEndpoint.isAlive()) {
+            for (String topic : topics) {
+                var actualTopic = actualTopics.get(topic.toLowerCase());
+                if (!actualTopic.isAlive()) {
+                    logger.warn("Topic {} is not alive!", topic);
+                    topicsHealthy = false;
+                    break;
+                }
+            }
+        } else {
+            logger.warn("Topic Endpoint is not alive!");
+            return false;
+        }
+
+        return topicsHealthy;
+    }
+
+    private void populateActualTopics() {
+        actualTopics = new HashMap<>();
+        topicEndpoint.getNoopTopicSinks().forEach(sink -> actualTopics.put(sink.getTopic(), sink));
+        topicEndpoint.getNoopTopicSources().forEach(source -> actualTopics.put(source.getTopic(), source));
     }
 }
